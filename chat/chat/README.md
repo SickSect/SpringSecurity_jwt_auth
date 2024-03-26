d958c9501a3f184d8c57a45fc05e766518a91ac1
Ugina <sleepyboy906sb@gmail.com>
2024-03-27 01:46:56 +0300

    fkyway

--- chat/chat/README.md
+++ chat/chat/README.md
@@ -1,1 +1,7 @@
+# Scratch
 https://habr.com/ru/articles/784508/
+# flyware
+https://habr.com/ru/articles/141354/
+https://habr.com/ru/companies/otus/articles/506788/
+
+
--- chat/chat/src/main/resources/db/migration/V1_init.sql
+++ chat/chat/src/main/resources/db/migration/V1_init.sql
@@ -1,21 +1,19 @@
-create table users (
-                       id                    bigserial,
-                       username              varchar(30) not null unique,
-                       password              varchar(80) not null,
-                       email                 varchar(50) unique,
-                       primary key (id)
-);
+create table if not exists 'users' (
+                       'id'                    serial not null primary key,
+                       'username'              varchar(30) not null unique,
+                       'password'              varchar(80) not null,
+                       'email'                 varchar(50) unique
+)ENGINE=InnoDB DEFAULT CHARSET=UTF8;
 
-create table roles (
-                       id                    serial,
-                       name                  varchar(50) not null,
-                       primary key (id)
-);
+create table if not exists 'roles' (
+                       'id'                    serial not null primary key,
+                       'name'                  varchar(50) not null
+)ENGINE=InnoDB DEFAULT CHARSET=UTF8;
 
-CREATE TABLE users_roles (
-                             user_id               bigint not null,
-                             role_id               int not null,
+CREATE TABLE if not exists 'users_roles' (
+                             'user_id'               bigint not null,
+                             'role_id'               int not null,
                              primary key (user_id, role_id),
                              foreign key (user_id) references users (id),
                              foreign key (role_id) references roles (id)
-);
+)ENGINE=InnoDB DEFAULT CHARSET=UTF8;
